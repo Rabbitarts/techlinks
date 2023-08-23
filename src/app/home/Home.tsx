0635fbefabd501a0bee47850a47cd302b67445d9
Rabbitarts <artemchumachenko2607@email.com>
2023-08-23 16:34:30 +0300

    fix import

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -3,11 +3,11 @@
 import Circles from '@/components/ui/circles/Circles'
 import { FC } from 'react'
 import { ITypeLinks } from '../../assets/data/links/links.interface'
-import { ITypeMe } from '../api/me/route'
 import AboutMe from './about-me/AboutMe'
 import Grid from './grid/Grid'
 import RecentPosts from './recent-social/RecentPosts'
 
+import { ITypeMe } from '@/assets/data/me'
 import styles from './Home.module.scss'
 
 interface IHome {
