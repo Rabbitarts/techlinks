09b7af81fb53553d86289367d13adae5b63b01b4
Rabbitarts <artemchumachenko2607@email.com>
2023-08-23 16:20:50 +0300

    fix get me data

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,21 +1,6 @@
+import { meData } from '@/assets/data/me'
 import { NextResponse } from 'next/server'
 
-export interface ITypeMe {
-	siteName: string
-	avatar: string
-	description: string
-}
-
-export const me: ITypeMe = {
-	siteName: 'Tech Links',
-	avatar: '/avatar.jpg',
-	description:
-		'<p>My name is artem, I have been studying and doing web development for half a year now. I make mini copies of projects of big tech giants  </p>' +
-		'<p>Like everyone else I started with learning html, css, js. Then I started learning more advanced frameworks like React and writing on Next.js. </p>' +
-		'<p>And then I started making some good fullstack pet projects for my portfolio. </p>' +
-		'<p>The main mission is to create cool and interesting and beautiful projects for your development. Thank you for visiting this site 😊 </p>',
-}
-
 export async function GET() {
-	return NextResponse.json(me)
+	return NextResponse.json(meData)
 }
