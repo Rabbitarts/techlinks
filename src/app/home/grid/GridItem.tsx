fb7f9f60578a3d467087736ec2b0cbffdfb7507d
Rabbitarts <artemchumachenko2607@email.com>
2023-08-23 14:14:10 +0300

    First

--- src/app/home/grid/GridItem.tsx
+++ src/app/home/grid/GridItem.tsx
@@ -19,7 +19,7 @@ const GridItem: FC<{ item: ITypeLinks }> = ({ item }) => {
 				}}
 			>
 				{/* eslint-disable-next-line @next/next/no-img-element */}
-				<img src={item.icon.path} width={item.icon.width} />
+				<img src={item.icon.path} alt='' width={item.icon.width} />
 			</div>
 			<div className={styles.title}>{item.title}</div>
 			{item.isImportant && <AnimateBlock />}
